5148015644cde3818301a1979d905b813dd6716a
Chris Fretz <chrisfretz@gmail.com>
2025-05-29 10:07:20 -0400

    Configure 11ty to include admin folder in public folder

--- .eleventy.js
+++ .eleventy.js
@@ -4,11 +4,12 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime);
     });
-    
+
     return {
         dir: {
             input: "src",
